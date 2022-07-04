4119ceae05eb780aac654de61df0203695f60b30
ganatan <dannyganatan@gmail.com>
2022-07-04 11:42:36 +0200

    Improve angular environment

--- angular/src/app/modules/home/home.component.ts
+++ angular/src/app/modules/home/home.component.ts
@@ -9,10 +9,6 @@ import { environment } from '../../../environments/environment';
 })
 export class HomeComponent implements OnInit {
 
-  name = environment.application.name;
-  version = environment.application.version;
-  bootstrap = environment.application.bootstrap;
-
   constructor(private meta: Meta) {
   }
 
@@ -21,7 +17,7 @@ export class HomeComponent implements OnInit {
     this.meta.updateTag(
       {
         name: 'description',
-        content: 'This application was developed with Angular version 14.0.1 and bootstrap 5.1.3' +
+        content: 'This application was developed with Angular version 14.0.4 and bootstrap 5.1.3' +
           ' It applies Routing, Lazy loading, Server side rendering and Progressive Web App (PWA)'
       });
 
--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -1,9 +1,3 @@
 export const environment = {
-  production: false,
-  application:
-  {
-    name: 'angular-starter',
-    version: 'Angular 14.0.4',
-    bootstrap: 'Bootstrap 5.1.3',
-  }
+  production: false
 };
